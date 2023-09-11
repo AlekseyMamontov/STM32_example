acd5f63e3da2f82961373e456bdb874093cfc26b
Olekseii <35818127+AlekseyMamontov@users.noreply.github.com>
2023-09-11 22:51:05 +0300

    Update InitSTM32F042_cmsis.c

--- InitSTM32F042_cmsis.c
+++ InitSTM32F042_cmsis.c
@@ -1,1 +1,317 @@
+void init_controller_STM32F042(void){
+
+	/*registr ACR  str.69
+	Bit 4 PRFTBE: Prefetch buffer enable
+	0: Prefetch is disabled /
+	1: Prefetch is enabled
+    Bit 3 Reserved, must be kept at reset value.
+    Bits 2:0 LATENCY[2:0]: Latency
+    These bits represent the ratio of the SYSCLK (system clock) period to the flash access time.
+    000: Zero wait state, if SYSCLK ≤ 24 MHz
+    001: One wait state, if 24 MHz < SYSCLK ≤ 48 MHz
+	 * */
+	FLASH->ACR |= FLASH_ACR_PRFTBE | FLASH_ACR_LATENCY; // включаем буфер и частота 48мгц
+
+	NVIC_SetPriority(RCC_CRS_IRQn,0);
+	NVIC_EnableIRQ(RCC_CRS_IRQn);
+
+	RCC->CR |= RCC_CR_HSEON;  			//включаем генератор HSE
+	while(!(RCC->CR & RCC_CR_HSERDY)){};//ожидание готовности HSE
+    RCC->CR |= RCC_CR_CSSON; 	//следить за кварцем
+
+	RCC->CFGR2 |= RCC_CFGR2_PREDIV_DIV1;  // Prediv предделитель для PLL
+	//завести HSE на PLL и 8 Мгц x6 = 48 Мгц
+	RCC->CFGR |=(RCC_CFGR_PLLSRC_HSE_PREDIV | RCC_CFGR_PLLMUL6);
+
+	RCC->CR |= RCC_CR_PLLON;  //включаем PLL
+	while(!(RCC->CR & RCC_CR_PLLRDY));  // ожидание готовности PLL
+	RCC->CFGR |= RCC_CFGR_SW_PLL;  		// переводим тактирование от PLL
+
+	//MODIFY_REG(RCC->CFGR,RCC_CFGR_HPRE,RCC_CFGR_HPRE_DIV1);//AHB Prescaler /1
+	//MODIFY_REG(RCC->CFGR,RCC_CFGR_PPRE,RCC_CFGR_PPRE_DIV1);//APB1 Prescaler /1,
+
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; // включить блок syscfg
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;// управление питанием
+
+
+
+	/*
+	 GPIO port mode register (GPIOx_MODER)
+		Address offset:0x00
+		Reset value: 0x2800 0000 for port A
+		Reset value: 0x0000 0000 for other ports
+			00: Input mode (reset state)
+			01: General purpose output mode
+			10: Alternate function mode
+			11: Analog mode
+	 GPIO port output type register (GPIOx_OTYPER)
+	 Address offset: 0x04
+	 Reset value: 0x0000 0000
+			0: Output push-pull (reset state)
+			1: Output open-drain
+
+	GPIO port output speed register (GPIOx_OSPEEDR)
+		Address offset: 0x08
+		Reset value: 0x0C00 0000 (for port A)
+		Reset value: 0x0000 0000 (for other ports)
+			x0: Low speed
+			01: Medium speed
+			11: High speed
+
+	GPIO port pull-up/pull-down register (GPIOx_PUPDR)
+		Address offset: 0x0C
+		Reset value: 0x2400 0000 (for port A)
+		Reset value: 0x0000 0000 (for other ports)
+			00: No pull-up, pull-down
+			01: Pull-up
+			10: Pull-down
+			11: Reserved
+	GPIO port input data register (GPIOx_IDR)
+		Address offset: 0x10
+		Reset value: 0x0000 XXXX
+	GPIO port output data register (GPIOx_ODR)
+		Address offset: 0x14
+		Reset value: 0x0000 0000
+	GPIO port bit set/reset register (GPIOx_BSRR) Reset value: 0x0000 0000
+	GPIO alternate function low register (GPIOx_AFRL) Reset value: 0x0000 0000
+	GPIO port configuration lock register (GPIOx_LCKR) Reset value: 0x0000 0000
+
+	 */
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;// включить порт GPIOA
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // включить порт GPIOВ
+
+	// Выходы реле и светодиоды статус и реле - сбрасываем.
+
+	GPIOA->BRR = 0x00FF; // bit0-7 size 8bit Rele
+	GPIOB->BRR = 0x0003;// bit 0-1 Led_Status_Pin/Led_Error_Pin
+	//пины 0-7 порта  А7A6A5A4A3A2A1A0  на выход
+    GPIOA->MODER |=0x5555; //0b010101010101010101: General purpose output mode
+    //GPIOA->OTYPER &=0b1111111100000000;//0 - push-pull, reset default
+    GPIOA->OSPEEDR |= 0x5555;//0b0101010101010101  speed medium
+    // пины Led_Status_Pin bit 0| Led_Error_Pin bit 1
+    GPIOB->MODER |= 0x05;//0b0101 01 -General purpose output mode
+    GPIOB->OTYPER |=0x03; // 0b11 1 - open drain
+    GPIOB->OSPEEDR |= 0x05; //0101  speed medium
+
+    //id_can pull_down  b3-9
+	GPIOB->PUPDR |= 0xAAA80;//0b10101010101010000000;
+	//speed pull_down  pin A10,A9,A8
+	GPIOA->PUPDR |= 0x2A0000;//0b1010100000000000000000;
+
+/*
+   	CAN GPIO Configuration
+   	   	   PA11     ------> CAN_RX
+   	   	   PA12     ------> CAN_TX
+
+	GPIO configuration for CAN signals
+	   	   (1) Select AF mode (10) on PA11 and PA12
+	   	   (2) AF4 for CAN signals
+ 	GPIOA->MODER = (GPIOA->MODER & ~(GPIO_MODER_MODER11 | GPIO_MODER_MODER12)) |
+	  	  	  	   	   (GPIO_MODER_MODER11_1 | GPIO_MODER_MODER12_1);
+ 	GPIOA->AFR[1] = (GPIOA->AFR[1] &~ (GPIO_AFRH_AFRH3 | GPIO_AFRH_AFRH4)) |
+	  	  	  	     	 (4 << (3 * 4)) | (4 << (4 * 4));
+
+	 CAN master control register (CAN_MCR) Reset value: 0x0001 0002
+	 CAN master status register (CAN_MSR)  Reset value: 0x0000 0C02
+*/
+
+	// включить порт А
+	// SET_BIT(RCC->AHBENR, RCC_AHBENR_GPIOAEN);
+    uint32_t speed;
+
+	//   выставить порты на альтернативные функции 0x10
+	GPIOA->MODER |= (GPIO_MODER_MODER11_1 | GPIO_MODER_MODER12_1);
+	// скорость порта на максимум
+	GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEEDR11|GPIO_OSPEEDR_OSPEEDR12;
+	//  пины 11,12 включить модуль bxCAN
+	GPIOA->AFR[1] |= ((0x04 << GPIO_AFRH_AFSEL11_Pos) | (0x04 << GPIO_AFRH_AFSEL12_Pos));
+
+	// включить модуль Can
+	RCC->APB1ENR |= RCC_APB1ENR_CANEN;
+
+	// перевод в режим иницилизации
+	GPIOB->BSRR |= led_error; //on_error
+
+	SET_BIT(CAN->MCR, CAN_MCR_INRQ);
+	while ((CAN->MSR & CAN_MSR_INAK) == 0){};
+	// выход из сна.
+	CLEAR_BIT(CAN->MCR, CAN_MCR_SLEEP);
+	while (CAN->MSR & CAN_MSR_SLAK){};
+
+	/*// reset default
+	CLEAR_BIT(CAN->MCR, CAN_MCR_TTCM);// Clear 0 /Set 1 the time triggered communication mode
+	CLEAR_BIT(CAN->MCR, CAN_MCR_ABOM);// Clear 0 /Set 1 the automatic bus-off management
+	CLEAR_BIT(CAN->MCR, CAN_MCR_AWUM);// Clear 0 /Set 1 the automatic wake-up mode
+	CLEAR_BIT(CAN->MCR, CAN_MCR_NART);// Set 1/ Clear 0 the automatic retransmission
+	CLEAR_BIT(CAN->MCR, CAN_MCR_RFLM);// Set the receive FIFO locked mode
+	CLEAR_BIT(CAN->MCR, CAN_MCR_TXFP);// Set the transmit FIFO priority
+	*/
+
+	// Получение данных
+
+	can_id = ((GPIOB->IDR)&0b0000001111111000)>>3;// b3-b9
+	can_speed = ((GPIOA->IDR)&0x700)>>8;
+	//can_speed =0;
+	switch(can_speed){ // 48 Мгц.
+
+		case 0x01: speed = 0x001c0002;break; //1000kb
+		case 0x02: speed = 0x001b0003;break; //800kb
+		case 0x03: speed = 0x001c0005;break; //500kb
+		case 0x04: speed = 0x001c000b;break; //250kb
+		case 0x05: speed = 0x001c0017;break; //125kb
+		case 0x06: speed = 0x001c001d;break; //100kb
+		case 0x07: speed = 0x001c003b;break; //50kb
+		default: speed = 0x001c0005;break;
+
+	};
+
+	WRITE_REG(CAN->BTR,speed);
+
+/*	CAN filter master register (CAN_FMR)  Reset value: 0x2A1C 0E01
+ * 		Bit 0 FINIT: Filter initialization mode
+		Initialization mode for filter banks
+			0: Active filters mode.
+			1: Initialization mode for the filters.(reset default)
+
+	CAN filter mode register (CAN_FM1R)  Reset value: 0x0000 0000
+		     FBMx: Filter mode
+			Mode of the registers of Filter x.
+			0: Two 32-bit registers of filter bank x are in Identifier Mask mode.
+			1: Two 32-bit registers of filter bank x are in Identifier List mode.
+
+	CAN filter scale register (CAN_FS1R)  Reset value: 0x0000 0000
+		This register can be written only when the filter initialization mode is set (FINIT=1) in the
+			FSCx: Filter scale configuration
+			These bits define the scale configuration of Filters 13-0.
+			0: Dual 16-bit scale configuration
+			1: Single 32-bit scale configuration
+
+	CAN filter FIFO assignment register (CAN_FFA1R) Reset value: 0x0000 0000
+		This register can be written only when the filter initialization mode is set (FINIT=1) in the
+		FFAx: Filter FIFO assignment for filter x
+			0: Filter assigned to FIFO 0
+			1: Filter assigned to FIFO 1
+
+	CAN filter activation register (CAN_FA1R) Reset value: 0x0000 0000
+		FACTx: Filter active
+		The software sets this bit to activate Filter x. To modify the Filter x registers (CAN_FxR[0:7]),
+		the FACTx bit must be cleared or the FINIT bit of the CAN_FMR register must be set.
+			0: Filter x is not active
+			1: Filter x is active
+
+	Filter bank i register x (CAN_FiRx) (i = 0..13, x = 1, 2) Reset value: 0xXXXX XXXX
+		There are 14 filter banks, i= 0 to 13.
+		Each filter bank i is composed of two 32-bit registers,
+		CAN_FiR[2:1].
+		This register can only be modified when the FACTx bit of the CAN_FAxR register is cleared
+		or when the FINIT bit of the CAN_FMR register is set.
+
+			Bits 31:0 FB[31:0]: Filter bits
+
+			Each bit of the register specifies the level of the corresponding bit of the expected identifier.
+				0: Dominant bit is expected
+				1: Recessive bit is expected
+			Mask
+			Each bit of the register specifies whether the bit of the associated identifier register must
+			match with the corresponding bit of the expected identifier or not.
+				0: Do not care, the bit is not used for the comparison
+				1: Must match, the bit of the incoming identifier mu
+
+*/
+
+	id_rxPDO1 = rxPDO1 + can_id;
+	id_txPDO1 = txPDO1 + can_id;
+	id_rxSDO =  rxSDO + can_id;
+	heartbroken = 0x700 + can_id;
+
+
+	CAN->FMR |=  CAN_FMR_FINIT; /*  init mode */
+	CAN->FA1R |= (CAN_FA1R_FACT0|CAN_FA1R_FACT1);//Activate filter 0,1
+	CAN->FM1R |= (CAN_FM1R_FBM0 |CAN_FM1R_FBM1); //ID_list
+	CAN->FS1R  &=~(CAN_FS1R_FSC0|CAN_FS1R_FSC1);  //16bit
+	CAN->FFA1R &=~CAN_FFA1R_FFA0; // filtr0 -> FIFO0
+	CAN->FFA1R |= CAN_FFA1R_FFA1; // filtr1 -> FIFO1
+
+	CAN->sFilterRegister[0].FR1 = id_rxPDO1<< 5;
+	CAN->sFilterRegister[0].FR2 = 0;
+	CAN->sFilterRegister[1].FR1 = (id_rxSDO<<16 | heartbroken)<< 5;
+	CAN->sFilterRegister[1].FR2 = (0x80 << 16 | 0x100 << 16)<< 5;
+
+	CAN->FMR &=~ CAN_FMR_FINIT; /* Leave filter init */
+	CAN->IER |= CAN_IER_FMPIE0; /* Set FIFO0 message pending IT enable */
+
+
+	/*выйти в нормальный режим*/
+
+	CAN->MCR &=~ CAN_MCR_INRQ;
+	while((CAN->MSR & CAN_MSR_INAK)==CAN_MSR_INAK){}
+
+	GPIOB->BRR |=led_error; //off_error
+/*
+  	Bit 4 FMPIE1: FIFO1 message pending interrupt enable
+	Bit 1 FMPIE0: FIFO0 message pending interrupt enable
+	    0: No interrupt generated when state of FMP[1:0] bits are not 00b.
+	    1: Interrupt generated when state of FMP[1:0] bits are not 00b.
+*/
+
+    /* CAN interrupt Init */
+	NVIC_SetPriority(CEC_CAN_IRQn,2);
+	NVIC_EnableIRQ(CEC_CAN_IRQn);
+
+}
+
+void CEC_CAN_IRQHandler(void){
+
+	uint32_t id,dlc,gpio = 0;
+	uint8_t data0;
+
+		test_irq = 1;
+
+		if((CAN->RF0R & 0b0011) == 0) return;
+		// индефикатор = тип сообщения ? extd : std
+		id = CAN->sFIFOMailBox[0].RIR&0b0100?CAN->sFIFOMailBox[0].RIR >> 3:CAN->sFIFOMailBox[0].RIR >> 21;
+		dlc = CAN->sFIFOMailBox[0].RDTR &0b01111;// получение из регистра длины кадра
+		if((CAN->sFIFOMailBox[0].RIR & 0b010) == 0 && dlc != 0){// тип сообщения 0 - data / 1- rtr
+
+			//dlc = dlc>8? 8:dlc;
+			data0 = CAN->sFIFOMailBox[0].RDLR&0xff;
+
+			switch (id){
+
+				 case 0:
+				  if(dlc < 2) break;
+				  if(data0 == 0 || data0== can_id )
+				  status_NMT = (CAN->sFIFOMailBox[0].RDLR&0xff00)>>8;;
+				 break;
+
+				default:
+				 if(id != id_rxPDO1) break;
+				 if(status_NMT != NMT_start) break;
+				 if(dlc>1) mask_gpio = (CAN->sFIFOMailBox[0].RDLR&0xFF00)>>8;
+				 gpio |= ((~data0)&mask_gpio);
+				 gpio = gpio<<16;
+				 gpio |= (data0&mask_gpio);
+				 Block_rele->BSRR = gpio;
+			     break;
+			};
+		};
+		SET_BIT(CAN->RF0R, CAN_RF0R_RFOM0);//CAN->RF0R |= 0b0100000;  сообщение прочитано.
+
+};
+CAN_TxMailBox_TypeDef tx_mailbox;
+
+uint8_t CAN_transmit (CAN_TxMailBox_TypeDef *tx){
+
+	uint8_t n=0;
+
+	if ((CAN->TSR & CAN_TSR_TME0) == CAN_TSR_TME0){
+	}else if((CAN->TSR & CAN_TSR_TME1) == CAN_TSR_TME1){ n=1;
+	}else if((CAN->TSR & CAN_TSR_TME2) == CAN_TSR_TME2){ n=2;
+	}else return 1; //busy
+
+	CAN->sTxMailBox[n].TDTR = tx->TDTR;
+	CAN->sTxMailBox[n].TDLR = tx->TDLR;
+	CAN->sTxMailBox[n].TDHR = tx->TDHR;
+	CAN->sTxMailBox[n].TIR = tx->TIR ;
+	}
 
